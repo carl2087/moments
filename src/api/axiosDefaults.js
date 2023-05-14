c10954d7d69781e04bb2efab38cd8924a98ab427
Carl <carlelli5@hotmail.co.uk>
2023-05-14 13:52:31 +0000

    signup form added with css

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+// defines the base url which is the API url from heroku
+axios.defaults.baseURL = 'https://drf-api-carl.herokuapp.com/';
+// defines the data format the api is expecting multipart means images and text
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+// avoids CORS errors when sending cookies
+axios.defaults.withCredentials = true;
