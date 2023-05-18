8764ae0acb5f5de381d79658db4c3337f17d3db4
Carl <carlelli5@hotmail.co.uk>
2023-05-18 19:11:31 +0000

    added a custom hook to close burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,14 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
 
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -54,7 +57,7 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <NavLink to='/'>
                 <Navbar.Brand>
@@ -62,7 +65,11 @@ const NavBar = () => {
                 </Navbar.Brand>
             </NavLink>
             {currentUser && addPostIcon}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle
+            ref={ref}
+            onClick={() => setExpanded(!expanded)}
+            aria-controls="basic-navbar-nav"
+            />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
                 <NavLink className={styles.NavLink} activeClassName={styles.Active} exact to='/'>
