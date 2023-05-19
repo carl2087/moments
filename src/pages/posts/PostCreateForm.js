5f095a0f105a573e79ed051f24ed90cf294215d4
Carl <carlelli5@hotmail.co.uk>
2023-05-19 14:41:27 +0000

    added form to enable users to upload images

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -12,7 +12,9 @@ import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+import { Alert, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults"
 
 function PostCreateForm() {
 const [errors, setErrors] = useState({});
@@ -22,6 +24,9 @@ const [postData, setPostData] = useState({
     content: "",
     image: ""
 })
+
+const imageInput = useRef(null)
+const history = useHistory()
 
 const handleChange = (event) => {
     setPostData({
@@ -37,6 +42,25 @@ const handleChangeImage = (event) => {
             ...postData,
             image: URL.createObjectURL(event.target.files[0])
         })
+    }
+}
+
+const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+
+    formData.append('title', title)
+    formData.append('content', content)
+    formData.append('image', imageInput.current.files[0])
+
+    try {
+        const {data} = await axiosReq.post('/posts/', formData);
+        history.push(`/posts/${data.id}`)
+    } catch(err) {
+        console.log(err)
+        if (err.response?.status !== 401) {
+            setErrors(err.response?.data)
+        }
     }
 }
 
@@ -57,6 +81,12 @@ const textFields = (
             onChange={handleChange}
             />
         </Form.Group>
+
+        {errors?.title?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>{message}</Alert>
+            )
+        )}
+
         <Form.Group>
             <Form.Label>
                 Content
@@ -70,20 +100,25 @@ const textFields = (
             />
         </Form.Group>
 
+        {errors?.content?.map((message, idx) => (
+        <Alert key={idx} variant="warning">{message}</Alert>
+        )
+        )}
+
     <Button
     className={`${btnStyles.Button} ${btnStyles.Blue}`}
-    onClick={() => {}}
+    onClick={() => history.goBack()}
     >
-    cancel
+    Cancel
     </Button>
     <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-    create
+    Create
     </Button>
 </div>
 );
 
 return (
-<Form>
+<Form onSubmit={handleSubmit}>
     <Row>
     <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
         <Container
@@ -113,8 +148,19 @@ return (
                 </Form.Label>                
             )}
 
-            <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage} />
+            <Form.File
+            id="image-upload"
+            accept="image/*"
+            onChange={handleChangeImage}
+            ref={imageInput}
+            />
         </Form.Group>
+
+        {errors?.image?.map((message, idx) => (
+        <Alert key={idx} variant="warning">{message}</Alert>
+        )
+        )}
+
         <div className="d-md-none">{textFields}</div>
         </Container>
     </Col>
