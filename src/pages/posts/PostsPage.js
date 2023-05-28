ea2d386dd0131ced97396673af628f7c2a82b6be
Carl <carlelli5@hotmail.co.uk>
2023-05-28 11:17:40 +0000

    added popular profiles component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,7 @@ import Asset from "../../components/Asset"
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
@@ -81,7 +82,7 @@ function PostsPage({ message, filter = "" }) {
         )}
         </Col>
         <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
         </Col>
     </Row>
     );
